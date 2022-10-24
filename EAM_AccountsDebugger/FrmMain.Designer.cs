27222466e90d40cdb4aba522bd9d57c1cb9b0cab
MaikEight <mail@maik8.de>
2022-10-25 00:49:18 +0200

    Initial commit
    Signed-off-by: MaikEight <mail@maik8.de>

---
+++ EAM_AccountsDebugger/FrmMain.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace EAM_AccountsDebugger
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnChangePath = new System.Windows.Forms.Button();
+            this.rtbOut = new System.Windows.Forms.RichTextBox();
+            this.pRtb = new System.Windows.Forms.Panel();
+            this.pRtb.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(13, 27);
+            this.tbPath.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(394, 23);
+            this.tbPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select your EAM.accounts file";
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Image = global::EAM_AccountsDebugger.Properties.Resources.icons8_forward_18px;
+            this.btnStart.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStart.Location = new System.Drawing.Point(13, 106);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(113, 27);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start process";
+            this.btnStart.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnChangePath
+            // 
+            this.btnChangePath.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChangePath.Image = global::EAM_AccountsDebugger.Properties.Resources.ic_folder_open_black_24dp;
+            this.btnChangePath.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnChangePath.Location = new System.Drawing.Point(13, 56);
+            this.btnChangePath.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnChangePath.Name = "btnChangePath";
+            this.btnChangePath.Size = new System.Drawing.Size(113, 27);
+            this.btnChangePath.TabIndex = 1;
+            this.btnChangePath.Text = "Change Path";
+            this.btnChangePath.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnChangePath.UseVisualStyleBackColor = true;
+            this.btnChangePath.Click += new System.EventHandler(this.btnChangePath_Click);
+            // 
+            // rtbOut
+            // 
+            this.rtbOut.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.rtbOut.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbOut.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbOut.Font = new System.Drawing.Font("Cascadia Code", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbOut.Location = new System.Drawing.Point(0, 0);
+            this.rtbOut.Name = "rtbOut";
+            this.rtbOut.ReadOnly = true;
+            this.rtbOut.Size = new System.Drawing.Size(392, 351);
+            this.rtbOut.TabIndex = 3;
+            this.rtbOut.Text = "";
+            // 
+            // pRtb
+            // 
+            this.pRtb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pRtb.Controls.Add(this.rtbOut);
+            this.pRtb.Location = new System.Drawing.Point(13, 139);
+            this.pRtb.Name = "pRtb";
+            this.pRtb.Size = new System.Drawing.Size(394, 353);
+            this.pRtb.TabIndex = 4;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(420, 504);
+            this.Controls.Add(this.pRtb);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btnChangePath);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(8)))), ((int)(((byte)(8)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FrmMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EAM Accounts Debugger by Maik8";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FrmMain_Paint);
+            this.pRtb.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangePath;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox rtbOut;
+        private System.Windows.Forms.Panel pRtb;
+    }
+}
+
---
+++ EAM_AccountsDebugger/FrmMain.cs
@@ -0,0 +1,138 @@
+﻿using MK_EAM_Lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAM_AccountsDebugger
+{
+    public partial class FrmMain : Form
+    {
+        public static readonly string saveFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ExaltAccountManager");
+        public readonly string accountsPath = Path.Combine(saveFilePath, "EAM.accounts");
+
+        BindingList<MK_EAM_Lib.AccountInfo> accounts = new BindingList<MK_EAM_Lib.AccountInfo>();
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            tbPath.Text = accountsPath;
+        }
+
+        private void FrmMain_Paint(object sender, PaintEventArgs e)
+        {
+            int h = ((btnStart.Top - btnChangePath.Bottom) / 2) + btnChangePath.Bottom;
+
+            using (Pen p = new Pen(this.ForeColor, 1f))
+            {
+                e.Graphics.DrawLine(p, btnChangePath.Left, h, tbPath.Right, h);
+            }
+        }
+
+        private void btnChangePath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog()
+            {
+                Title = "Please select your EAM.accounts file.",
+                Multiselect = false,
+                InitialDirectory = accountsPath,
+                Filter = "Accounts files (EAM.accounts)|EAM.accounts|All files (*.*)|*.*"
+            };
+
+            if (diag.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = diag.FileName;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            rtbOut.Clear();
+            tbPath.ReadOnly = true;
+
+            Log("Start processing the file: " + tbPath.Text);
+
+            if (!File.Exists(tbPath.Text))
+            {
+                Log("Can't find the selected file, aborting.");
+                Log("Please try again.");
+
+                tbPath.ReadOnly = false;
+                return;
+            }
+            Log("File found, reading...");
+
+            try
+            {
+                byte[] data = File.ReadAllBytes(tbPath.Text);
+                Log("Reading: Success");
+                Log("Converting to AccountSaveFile...");
+                AccountSaveFile saveFile = (AccountSaveFile)ByteArrayToObject(data);
+                Log("AccountSaveFile: Success");
+
+                Log($"AccountSaveFile:{Environment.NewLine}" +
+                    $"Version: {saveFile.version}{Environment.NewLine}" +
+                    $"Error: {saveFile.error}{Environment.NewLine}" +
+                    $"Entropy: {saveFile.entropy.Length}");
+
+                Log("Decrypting saveFile...");
+                List<AccountInfo> acc = AccountSaveFile.Decrypt(saveFile);
+                Log("Decrypting: Success");
+
+                Log($"AccountInfos count: {acc.Count}");
+
+                Log("Creating a new BindingList with AccountInfos...");
+                accounts = new BindingList<MK_EAM_Lib.AccountInfo>(acc);
+                Log("BindingList creation: Success" + Environment.NewLine);
+
+                Log("Accountlist:");
+
+                for (int i = 0; i < accounts.Count; i++)
+                {
+                    Log(accounts[i].email);
+                    Log(accounts[i].password + Environment.NewLine);
+                }
+
+                Log("Done!");
+            }
+            catch (Exception ex)
+            {
+                Log("Caught an exception: " + Environment.NewLine + ex.Message + Environment.NewLine + Environment.NewLine + ex.StackTrace);
+                Log("Please scramble any account-related informations and forward this log to Maik8.");
+            }
+            finally
+            {
+                tbPath.ReadOnly = false;
+            }
+        }
+
+        private void Log(string msg)
+        {
+            rtbOut.AppendText($"{DateTime.Now.ToString("HH:mm:ss")}: {msg}{Environment.NewLine}");
+        }
+
+        public object ByteArrayToObject(byte[] arrBytes)
+        {
+            using (MemoryStream memStream = new MemoryStream())
+            {
+                BinaryFormatter binForm = new BinaryFormatter();
+                memStream.Write(arrBytes, 0, arrBytes.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                object obj = (object)binForm.Deserialize(memStream);
+
+                return obj;
+            }
+        }
+    }
+}
